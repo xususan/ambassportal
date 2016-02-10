07061900c02f4fca6d8fd5520e7ef1d56a6d4cea
Melissa Yu <themelissayu@gmail.com>
2016-02-10 17:08:39 -0500

    added missing directory

---
+++ ambassportal/ambassportal/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.contrib.flatpages import views
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'ambassportal.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    #url(r'^$', views.populate_home_page),
+    #url(r'^profile/', views.populate_profile),
+    url(r'^admin/', include(admin.site.urls)),
+    url('', include('social.apps.django_app.urls', namespace='social')),
+    url(r'^(?P<url>.*/)$', views.flatpage),
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ ambassportal/userprofile/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+import django.contrib.auth.models as authmodels
+import os
+
+def rename_file(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = os.path.join(os.getcwd(), 'imgs/profiles/%s.%s' % (instance.username, ext))
+    return filename
+    
+class User(authmodels.User):
+    supervisor_id = models.CharField(max_length=30)
+    college = models.CharField(max_length=30)
+    points = models.IntegerField()
+    prof_pic = models.ImageField(upload_to=rename_file, max_length=300)
+
