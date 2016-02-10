7d778b8c93430e686a6eafa4ceda10555dd82798
xususan <susan.xu97@gmail.com>
2016-02-09 22:56:41 -0500

    new userprofile app in py

--- ambassportal/urls.py
+++ ambassportal/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^userprofile/', include('userprofile.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
