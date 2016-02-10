56611f2646392a99d1b93f7fe31317bca8d003f4
xususan <susan.xu97@gmail.com>
2016-02-10 03:10:23 -0500

    added new apps

--- userprofile/models.py
+++ userprofile/models.py
@@ -3,3 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class UserInfo(models.Model):
+	name = 
+	points =
+	photo =
+	university =
+	ambassador_lead =
+
+	
