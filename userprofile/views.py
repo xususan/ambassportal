7ed88a7cc2fb8176063a324f34a26610645d607f
xususan <susan.xu97@gmail.com>
2016-02-10 16:15:23 -0500

    views change

--- userprofile/views.py
+++ userprofile/views.py
@@ -5,5 +5,21 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+from django.http import HttpResponse
+
+import requests
+
+def testRequest(request):
+    headers = {
+    "content-type": "application/vnd.api+json",
+    "x-user-location": "[54,12]"
+    }
+    res = requests.get("https://masu-api-staging.sngp.co/ambassador/supervisors", headers=headers)
+    # print(res.status_code, res.reason)
+    return res
+
 def index(request):
-	return render(request,'userprofile/index.html')
+    return HttpResponse(testRequest(request).text)
+
+#def index(request):
+	#return render(request,'userprofile/index.html')
